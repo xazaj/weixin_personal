ea65758c587b6befffa6badce61622e417713796
xazaj <zhuaijun@gmail.com>
2013-05-04 21:18:55 +0800

    起航

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'http://ruby.taobao.org'
+
+gem 'rails', '3.2.11'
+
+group :development do
+	gem 'sqlite3', '1.3.5'
+	gem 'rspec-rails', '2.11.0'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails', '2.0.2'
+
+group :test do
+	gem 'capybara', '1.1.2'
+end
+
+group :production do
+	gem 'pg', '0.12.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeixinPersonal::Application.config.secret_token = 'e31f785d720facf7aee2d9f9aa2cec772710e462fc309af29692f320aeb7ce6bdb5d55cd46bc59ff0eb7f0fd100844a11a8ee7b025cda2cfa25291e224bf896a'
